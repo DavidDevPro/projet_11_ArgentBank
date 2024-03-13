e3e7bd612efad8ad96bef6d82c708cd1d8bacbd5
David Changea <14ir155@gmail.com>
2024-03-13 18:50:17 +0100

    avancée sur le formulaire

--- FrontEnd/src/components/FormLogin.jsx
+++ FrontEnd/src/components/FormLogin.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
 
 const FormLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remenberMe, setRemenberMe] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  };
+
+  const handleRemenberMe = (e) => {
+    setRemenberMe(e.target.checked);
+  };
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
@@ -12,22 +23,38 @@ const FormLogin = () => {
           className="fa fa-user-circle sign-in-icon"
         />
         <h1>Sign In</h1>
-        <form action="">
+        <form onSubmit={handleLogin}>
           <div className="input-wrapper">
             <label htmlFor="usermail">User Mail:</label>
-            <input type="text" id="usermail" />
+            <input
+              type="text"
+              id="usermail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="exemple@gmail.com"
+              required
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password:</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
           </div>
           <div className="input-remember">
-            <input type="checkbox" id="remember-me" />
+            <input
+              type="checkbox"
+              id="remember-me"
+              checked={remenberMe}
+              onChange={handleRemenberMe}
+            />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          <Link to="/user" className="sign-in-button">
-            Sign In
-          </Link>
+          <button className="sign-in-button">Sign In</button>
         </form>
       </section>
     </main>
