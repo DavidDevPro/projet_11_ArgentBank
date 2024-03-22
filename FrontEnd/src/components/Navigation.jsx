c5b0c5e732ab2dc846baf52983253051ad319204
David Changea <14ir155@gmail.com>
2024-03-22 12:50:05 +0100

    gestion login/logout terminé

--- FrontEnd/src/components/Navigation.jsx
+++ FrontEnd/src/components/Navigation.jsx
@@ -1,9 +1,30 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../assets/images/argentBankLogo.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+// redux
+import { useDispatch, useSelector } from "react-redux";
 
 const Navigation = () => {
+  const token = useSelector((state) => state.Login.token);
+  const loginStore = useSelector((state) => state.Login);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch({
+      type: "Login/userToken",
+      payload: null,
+    });
+    dispatch({
+      type: "Login/userInfos",
+      payload: null,
+    });
+    navigate("/");
+  };
+
   return (
     <nav className="main-nav">
       <NavLink to="/" className="main-nav-logo">
@@ -15,10 +36,27 @@ const Navigation = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
-        <NavLink to="/login" className="main-nav-item">
-          <FontAwesomeIcon icon={faCircleUser} />
-          Sign In
-        </NavLink>
+        {token ? (
+          loginStore &&
+          loginStore.userInfos &&
+          loginStore.userInfos.userName && (
+            <div className="main-nav-logout">
+              <p>{loginStore.userInfos.userName}</p>
+              <p className="logout" onClick={handleLogout}>
+                <FontAwesomeIcon
+                  className="icon-logout"
+                  icon={faRightFromBracket}
+                />
+                Sign Out
+              </p>
+            </div>
+          )
+        ) : (
+          <NavLink to="/login" className="main-nav-item">
+            <FontAwesomeIcon icon={faCircleUser} />
+            Sign In
+          </NavLink>
+        )}
       </div>
     </nav>
   );
